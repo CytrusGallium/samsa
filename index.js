114892855d997638d6cce3d11ba4a24d070e28d3
Abdeldjelil BABAHAMED <8852906+CytrusGallium@users.noreply.github.com>
2023-07-15 02:33:19 +0100

    Just a web socket server

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
 const WebSocket = require('ws');
-const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors');
-const net = require('net');
 
 console.log('======================================================================');
-console.log('========================== Samsa SMS API =============================');
+console.log('======================== Samsa SMS Gateway ===========================');
 console.log('======================================================================');
 
-// Init express app
-const app = express();
-
-// Cors middleware
-app.use(cors({
-    origin: '*'
-}));
-
-// ==================================================================================
-// Web Socket
-// ==================================================================================
+// Host WebSocket Server
 const webSocketServer = new WebSocket.Server({ port: 9999 });
-let workers = [];
+
+
+let sockets = [];
+
 webSocketServer.on('connection', (socket) => {
     console.log("Client connected.");
-    workers.push(socket);
+    sockets.push(socket);
     socket.send("Hello, Client !");
     // When you receive a message, send that message to every socket.
     socket.on('message', (msg) => {
@@ -32,39 +22,12 @@ webSocketServer.on('connection', (socket) => {
     });
     // When a socket closes, or disconnects, remove it from the array.
     socket.on('close', () => {
-        workers = workers.filter(s => s !== socket);
+        sockets = sockets.filter(s => s !== socket);
     });
 });
 // Websocket End =====================================================================
 
-// Base Route
-app.get("/", (req, res) => {
-    res.status(200).send("Welcome to Nukode's Samsa SMS Sending API. In order to send an sms please use the /send route, while providing the phone and message parameters.");
-});
-
-// Send Route
-app.get("/send", (req, res) => {
-
-    if (req.query.phone && req.query.message) {
-        if (workers && workers.length > 0) {
-            workers[0].send({ phone: req.query.phone, message: req.query.message });
-        }
-        else {
-            res.status(200).send("NO_WORKER"); // TODO : API error for not enough ressources
-        }
-
-    } else {
-        res.status(200).send("INVALID_RECEPIENT"); // TODO : API error for not enough ressources
-    }
-
-});
-
-app.listen(9000, () => {
-    console.log("Samsa SMS API started on port 9000");
-    console.log('======================================================================');
-});
-
-const url = 'ws://localhost:9999/';
+const url = 'wss://apple-fan-ink.glitch.me/';
 let wsClient = new WebSocket(url, {
     headers: {
         "User-Agent": `WebSocket Client`
