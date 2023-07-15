ab0d855bf5419d00e1f01c51a1c5922ebf960ff6
Abdeldjelil BABAHAMED <8852906+CytrusGallium@users.noreply.github.com>
2023-07-15 17:48:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,41 +8,70 @@ console.log('===================================================================
 // Host WebSocket Server
 const webSocketServer = new WebSocket.Server({ port: 9999 });
 
-
 let sockets = [];
+let providers = [];
+let consumers = [];
 
+// Websocket Setup Start =====================================================================
 webSocketServer.on('connection', (socket) => {
+
+    // Welcome newly connected client
     console.log("Client connected.");
     sockets.push(socket);
-    socket.send("Hello, Client !");
-    // When you receive a message, send that message to every socket.
+    socket.send('{code:"WELCOME"}');
+
+    // When you receive a message, process it.
     socket.on('message', (msg) => {
-        // sockets.forEach(s => s.send(msg));
-        console.log("WebSocket Message : " + msg);
+        console.log("Message from Client : " + msg);
+
+        const json = JSON.parse(msg);
+
+        if (json && json.code) {
+
+            if (json.code === "ME_CONSUMER") {
+                Process_ME_CONSUMER(json, socket);
+            }
+            else if (json.code === "ME_PROVIDER") {
+                Process_ME_PROVIDER(json, socket);
+            }
+            else if (json.code === "SEND") {
+                Process_SEND(json, socket);
+            }
+        }
     });
+
     // When a socket closes, or disconnects, remove it from the array.
     socket.on('close', () => {
         sockets = sockets.filter(s => s !== socket);
     });
 });
-// Websocket End =====================================================================
+// Websocket Setup End ====================================================================
 
-const url = 'wss://apple-fan-ink.glitch.me/';
-let wsClient = new WebSocket(url, {
-    headers: {
-        "User-Agent": `WebSocket Client`
+const Process_ME_CONSUMER = (ParamJson, ParamSocket) => {
+    console.log("Registering consumer : " + ParamSocket._socket.remoteAddress);
+    consumers.push(ParamSocket);
+}
+
+const Process_ME_PROVIDER = (ParamJson, ParamSocket) => {
+    console.log("Registering provider : " + ParamSocket._socket.remoteAddress);
+    providers.push(ParamSocket);
+}
+
+const Process_SEND = (ParamJson, ParamSocket) => {
+
+    if (providers && providers.length > 0) {
+
+        // ...
+        console.log("Sending message from : " + ParamSocket._socket.remoteAddress);
+
+        // ...
+        if (ParamJson && ParamJson.phone && ParamJson.message) {
+            console.log("Sending Message : " + ParamJson.message);
+            console.log("Sending To : " + ParamJson.phone);
+        }
     }
-});
+    else {
+        console.log("No providers to send message through :(");
+    }
 
-wsClient.on('open', () => {
-    console.log(`Connected to ${url}`);
-    wsClient.send('Hello Server ! This is a Test.');
-});
-
-// wsClient.on('message', (data) => {
-//     console.log(`Message from server: ${data}`);
-// });
-
-// wsClient.on('error', (err) => {
-//     console.log(`WebSocket error: ${err}`);
-// });
+}
